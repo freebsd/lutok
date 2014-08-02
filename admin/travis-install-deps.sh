9276e47e96db8423dea96f5f0b6f10954727035d
Julio Merino <jmmv@google.com>
2014-08-01 22:24:32 -0400

    Try to fully set up Kyua in Travis CI
    This is so that we can run the full test suite.  Didn't want to do this
    yet, but yet, it's not that hard and we'll need this for running Kyua
    later on.

--- admin/travis-install-deps.sh
+++ admin/travis-install-deps.sh
@@ -3,13 +3,33 @@
 set -e -x
 
 sudo apt-get update -qq
-sudo apt-get install -y doxygen liblua5.2-0 liblua5.2-dev pkg-config
+sudo apt-get install -y doxygen liblua5.2-0 liblua5.2-dev \
+    libsqlite3-0 libsqlite3-dev pkg-config sqlite3
 
-wget --no-check-certificate \
-    https://github.com/jmmv/atf/releases/download/atf-0.20/atf-0.20.tar.gz
-tar -xzvf atf-0.20.tar.gz
-( cd atf-0.20 && ./configure --prefix=/usr && make && sudo make install )
+install_from_github() {
+    local project="${1}"; shift
+    local name="${1}"; shift
+    local release="${1}"; shift
 
-# TODO(jmmv): Should also install Kyua to be able to run the tests... but
-# without a binary package available, the circular dependencies become hard
-# to handle.
+    local distname="${name}-${release}"
+
+    local baseurl="https://github.com/jmmv/${project}"
+    wget --no-check-certificate \
+        "${baseurl}/releases/download/${distname}/${distname}.tar.gz"
+    tar -xzvf "${distname}.tar.gz"
+
+    cd "${distname}"
+    ./configure \
+        --disable-developer \
+        --prefix=/usr \
+        --without-atf \
+        --without-doxygen
+    make
+    sudo make install
+    cd -
+}
+
+install_from_github atf atf 0.20
+install_from_github lutok lutok 0.4
+install_from_github kyua kyua-testers 0.2
+install_from_github kyua kyua-cli 0.8
