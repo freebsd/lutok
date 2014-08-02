19fa3590d64fec60034a79c1e49b93a3899cd358
Julio Merino <jmmv@google.com>
2014-08-01 21:59:59 -0400

    Forbid creating a distfile when Doxygen is missing
    "make distcheck" was strangely failing when Doxygen was not present
    even when the install rules check for this condition.  This was because
    the dist rules were not handling the lack of Doxygen properly.
    
    Discovered while trying to set up Travis CI for lutok.

--- admin/travis-install-deps.sh
+++ admin/travis-install-deps.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 sudo apt-get update -qq
-sudo apt-get install -y liblua5.2-0 liblua5.2-dev pkg-config
+sudo apt-get install -y doxygen liblua5.2-0 liblua5.2-dev pkg-config
 
 wget --no-check-certificate \
     https://github.com/jmmv/atf/releases/download/atf-0.20/atf-0.20.tar.gz
